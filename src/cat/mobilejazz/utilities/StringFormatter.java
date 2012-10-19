fdb419eb06b4712eee4681249aaec8f440fc61b3
Hannes Widmoser <widmoser@gmail.com>
2012-10-19 18:13:47 +0200

    Added a method to append whitespaces

--- src/cat/mobilejazz/utilities/StringFormatter.java
+++ src/cat/mobilejazz/utilities/StringFormatter.java
@@ -2,10 +2,16 @@ package cat.mobilejazz.utilities;
 
 import java.util.Iterator;
 
-
 public class StringFormatter {
+
+	private static final String INDENT_LITERAL_32 = "                                                                                                                                    ";
+
+	public static StringBuilder appendWS(StringBuilder b, int num) {
+		return b.append(INDENT_LITERAL_32, 0, num);
+	}
 	
-	public static <V> CharSequence printIterable(Iterable<V> iterable, CharSequence delimiter, ObjectPrinter<V> printer) {
+	public static <V> CharSequence printIterable(Iterable<V> iterable,
+			CharSequence delimiter, ObjectPrinter<V> printer) {
 		StringBuilder output = new StringBuilder();
 		Iterator<V> i = iterable.iterator();
 		if (i.hasNext()) {
@@ -17,17 +23,20 @@ public class StringFormatter {
 		}
 		return output;
 	}
-	
+
 	@SuppressWarnings("unchecked")
-	public static  <V> CharSequence printIterable(Iterable<V> iterable, CharSequence delimiter) {
-		return printIterable(iterable, delimiter, (ObjectPrinter<V>)DefaultObjectPrinter.getInstance());
+	public static <V> CharSequence printIterable(Iterable<V> iterable,
+			CharSequence delimiter) {
+		return printIterable(iterable, delimiter,
+				(ObjectPrinter<V>) DefaultObjectPrinter.getInstance());
 	}
-	
+
 	public static <V> CharSequence printIterable(Iterable<V> iterable) {
 		return printIterable(iterable, ", ");
 	}
-	
-	public static <V> CharSequence printIterable(CharSequence delimiter, ObjectPrinter<V> printer, V... array) {
+
+	public static <V> CharSequence printIterable(CharSequence delimiter,
+			ObjectPrinter<V> printer, V... array) {
 		StringBuilder output = new StringBuilder();
 		if (array.length > 0) {
 			output.append(printer.toString(array[0]));
@@ -38,12 +47,14 @@ public class StringFormatter {
 		}
 		return output;
 	}
-	
+
 	@SuppressWarnings("unchecked")
-	public static  <V> CharSequence printIterable(CharSequence delimiter, V... array) {
-		return printIterable(delimiter, (ObjectPrinter<V>)DefaultObjectPrinter.getInstance(), array);
+	public static <V> CharSequence printIterable(CharSequence delimiter,
+			V... array) {
+		return printIterable(delimiter,
+				(ObjectPrinter<V>) DefaultObjectPrinter.getInstance(), array);
 	}
-	
+
 	public static <V> CharSequence printIterable(V... array) {
 		return printIterable(", ", array);
 	}
