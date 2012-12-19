2395a08fa557fbcd9d42e8c60e7f8233f8da2116
Hannes Widmoser <widmoser@gmail.com>
2012-12-19 14:25:10 +0100

    Added an optional empty resource that is displayed, when no elements are in the list.

--- src/cat/mobilejazz/utilities/data/adapter/ListAdapter.java
+++ src/cat/mobilejazz/utilities/data/adapter/ListAdapter.java
@@ -14,17 +14,27 @@ public abstract class ListAdapter<T> extends BaseAdapter {
 	protected List<T> data;
 	protected Context context;
 	private int resourceId;
+	private int resourceIdEmpty;
 	private LayoutInflater inflater;
 
-	public ListAdapter(List<T> data, int resourceId, Context context) {
+	public ListAdapter(List<T> data, int resourceId, int resourceIdEmpty, Context context) {
 		if (data != null) {
 			this.data = data;
 		} else {
 			this.data = new ArrayList<T>();
 		}
 		this.resourceId = resourceId;
+		this.resourceIdEmpty = resourceIdEmpty;
 		this.inflater = (LayoutInflater) context.getSystemService(Context.LAYOUT_INFLATER_SERVICE);
 		this.context = context;
+	}
+	
+	public ListAdapter(List<T> data, int resourceId, Context context) {
+		this(data, resourceId, 0, context);
+	}
+	
+	public ListAdapter(int resourceId, Context context) {
+		this(null, resourceId, 0, context);
 	}
 
 	protected abstract int getObjectId(T obj);
@@ -33,25 +43,42 @@ public abstract class ListAdapter<T> extends BaseAdapter {
 
 	@Override
 	public int getCount() {
-		return data.size();
+		if (!data.isEmpty()) {
+			return data.size();
+		} else {
+			return resourceIdEmpty > 0 ? 1 : 0;
+		}
 	}
 
 	@Override
 	public Object getItem(int position) {
-		return data.get(position);
+		if (!data.isEmpty()) {
+			return data.get(position);
+		} else {
+			return null;
+		}
 	}
 
 	@Override
 	public long getItemId(int position) {
-		return getObjectId(data.get(position));
+		if (!data.isEmpty()) {
+			return getObjectId(data.get(position));
+		} else {
+			return -1;
+		}
 	}
 
 	@Override
 	public View getView(int position, View convertView, ViewGroup parent) {
 		View view = convertView;
-		if (view == null)
-			view = inflater.inflate(resourceId, null);
-		updateView(view, data.get(position));
+		if (!data.isEmpty()) {
+			if (view == null || view.getId() != resourceId)
+				view = inflater.inflate(resourceId, null);
+			updateView(view, data.get(position));
+		} else {
+			if (view == null || view.getId() != resourceIdEmpty)
+				view = inflater.inflate(resourceIdEmpty, null);
+		}
 		return view;
 	}
 
