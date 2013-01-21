d04306c8d46016a0364d642d4addb5c1840ce905
Hannes Widmoser <widmoser@gmail.com>
2013-01-21 17:34:28 +0100

    outsourced some classes to mobilejazz-android-controller and mobilejazz-android-views

--- src/cat/mobilejazz/utilities/io/IOUtils.java
+++ src/cat/mobilejazz/utilities/io/IOUtils.java
@@ -6,7 +6,27 @@ import java.io.InputStreamReader;
 import java.io.OutputStream;
 import java.io.Reader;
 
+/**
+ * Provides static convenience methods to handle {@link InputStream}s and
+ * {@link Reader}s.
+ * 
+ * @author Hannes Widmoser
+ * 
+ */
 public class IOUtils {
+
+	/**
+	 * A listener interface to asynchronously notify progress of a long running
+	 * operation such as copying from one stream to another.
+	 * 
+	 * @author Hannes Widmoser
+	 * 
+	 */
+	public static interface ProgressListener {
+
+		public void onProgress(float progress);
+
+	}
 
 	/**
 	 * Reads from a {@link Reader} and outputs the results to a
@@ -31,10 +51,43 @@ public class IOUtils {
 		return result;
 	}
 
+	/**
+	 * Reads from an {@link InputStream} and outputs the results to a
+	 * {@link CharSequence}. Note that the reader is closed after reaching the
+	 * end of the input.
+	 * 
+	 * @param in
+	 *            The input {@link InputStream}
+	 * @return A {@link CharSequence} representing the contents of the stream
+	 *         object.
+	 * @throws IOException
+	 *             if the some IO error occurred.
+	 */
 	public static CharSequence read(InputStream in) throws IOException {
 		return read(new InputStreamReader(in));
 	}
 
+	/**
+	 * Copies the content of an {@link InputStream} to a different
+	 * {@link OutputStream}.
+	 * 
+	 * @param from
+	 *            The source.
+	 * @param to
+	 *            The target.
+	 * @param chunkSize
+	 *            The maximum number of bytes that should be read/written at
+	 *            once (i.e. with one operation). See
+	 *            {@link InputStream#read(byte[])} for more information.
+	 * @param listener
+	 *            A {@link ProgressListener} that is informed of the progress of
+	 *            the operation. May be {@code null}.
+	 * @param fileSize
+	 *            The expected number of bytes from the input. If a listener is
+	 *            set, this needs to be greater than 0. Otherwise it is ignored.
+	 * @throws IOException
+	 *             If the operation fails.
+	 */
 	public static void copy(InputStream from, OutputStream to, int chunkSize, ProgressListener listener, long fileSize)
 			throws IOException {
 		try {
