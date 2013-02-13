b88bc0f74911dff233c1d5122d08b454b023f247
Hannes Widmoser <widmoser@gmail.com>
2013-02-13 16:29:30 +0100

    added doc comments

--- src/cat/mobilejazz/utilities/collections/LimitedSizeHashMap.java
+++ src/cat/mobilejazz/utilities/collections/LimitedSizeHashMap.java
@@ -2,6 +2,17 @@ package cat.mobilejazz.utilities.collections;
 
 import java.util.LinkedHashMap;
 
+/**
+ * 
+ * A hash map that does not exceed a maximum number of elements. When the map
+ * contains the maximum number of elements and a new element is added, the
+ * oldest one is removed to make space for the new one.
+ * 
+ * @author Hannes Widmoser
+ * 
+ * @param <K>
+ * @param <V>
+ */
 public class LimitedSizeHashMap<K, V> extends LinkedHashMap<K, V> {
 
 	/**
