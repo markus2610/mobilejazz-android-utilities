b39aeac47d453daa1ceba9e878ead60ad757e75d
Hannes Widmoser <widmoser@gmail.com>
2013-02-18 10:14:15 +0100

    gracefully handling null arrays/lists in formatting methods
    when passing null, null is returned

--- src/cat/mobilejazz/utilities/format/StringFormatter.java
+++ src/cat/mobilejazz/utilities/format/StringFormatter.java
@@ -9,7 +9,7 @@ public class StringFormatter {
 	public static StringBuilder appendWS(StringBuilder b, int num) {
 		return b.append(INDENT_LITERAL_32, 0, num);
 	}
-	
+
 	public static CharSequence repeat(StringBuilder output, CharSequence content, CharSequence delimiter, int count) {
 		output.append(content);
 		for (int i = 1; i < count; ++i) {
@@ -17,37 +17,39 @@ public class StringFormatter {
 		}
 		return output;
 	}
-	
+
 	public static CharSequence repeat(StringBuilder output, CharSequence content, int count) {
 		return repeat(output, content, " ", count);
 	}
-	
+
 	public static CharSequence repeat(CharSequence content, CharSequence delimiter, int count) {
 		return repeat(new StringBuilder(), content, delimiter, count);
 	}
-	
+
 	public static CharSequence repeat(CharSequence content, int count) {
 		return repeat(new StringBuilder(), content, " ", count);
 	}
-	
-	public static <V> CharSequence printIterable(StringBuilder output, Iterable<V> iterable,
-			CharSequence delimiter, ObjectPrinter<V> printer) {
-		Iterator<V> i = iterable.iterator();
-		if (i.hasNext()) {
-			output.append(printer.toString(i.next()));
+
+	public static <V> CharSequence printIterable(StringBuilder output, Iterable<V> iterable, CharSequence delimiter,
+			ObjectPrinter<V> printer) {
+		if (iterable != null) {
+			Iterator<V> i = iterable.iterator();
+			if (i.hasNext()) {
+				output.append(printer.toString(i.next()));
+			}
+			while (i.hasNext()) {
+				output.append(delimiter);
+				output.append(printer.toString(i.next()));
+			}
+			return output;
+		} else {
+			return null;
 		}
-		while (i.hasNext()) {
-			output.append(delimiter);
-			output.append(printer.toString(i.next()));
-		}
-		return output;
 	}
 
 	@SuppressWarnings("unchecked")
-	public static <V> CharSequence printIterable(StringBuilder output, Iterable<V> iterable,
-			CharSequence delimiter) {
-		return printIterable(output, iterable, delimiter,
-				(ObjectPrinter<V>) DefaultObjectPrinter.getInstance());
+	public static <V> CharSequence printIterable(StringBuilder output, Iterable<V> iterable, CharSequence delimiter) {
+		return printIterable(output, iterable, delimiter, (ObjectPrinter<V>) DefaultObjectPrinter.getInstance());
 	}
 
 	public static <V> CharSequence printIterable(StringBuilder output, Iterable<V> iterable) {
@@ -56,34 +58,34 @@ public class StringFormatter {
 
 	public static <V> CharSequence printIterable(StringBuilder output, CharSequence delimiter,
 			ObjectPrinter<V> printer, V... array) {
-		if (array.length > 0) {
-			output.append(printer.toString(array[0]));
+		if (array != null) {
+			if (array.length > 0) {
+				output.append(printer.toString(array[0]));
+			}
+			for (int i = 1; i < array.length; i++) {
+				output.append(delimiter);
+				output.append(printer.toString(array[i]));
+			}
+			return output;
+		} else {
+			return null;
 		}
-		for (int i = 1; i < array.length; i++) {
-			output.append(delimiter);
-			output.append(printer.toString(array[i]));
-		}
-		return output;
 	}
 
 	@SuppressWarnings("unchecked")
-	public static <V> CharSequence printIterable(StringBuilder output, CharSequence delimiter,
-			V... array) {
-		return printIterable(output, delimiter,
-				(ObjectPrinter<V>) DefaultObjectPrinter.getInstance(), array);
+	public static <V> CharSequence printIterable(StringBuilder output, CharSequence delimiter, V... array) {
+		return printIterable(output, delimiter, (ObjectPrinter<V>) DefaultObjectPrinter.getInstance(), array);
 	}
 
 	public static <V> CharSequence printIterable(StringBuilder output, V... array) {
 		return printIterable(output, ", ", array);
 	}
-	
-	public static <V> CharSequence printIterable(Iterable<V> iterable,
-			CharSequence delimiter, ObjectPrinter<V> printer) {
+
+	public static <V> CharSequence printIterable(Iterable<V> iterable, CharSequence delimiter, ObjectPrinter<V> printer) {
 		return printIterable(new StringBuilder(), iterable, delimiter, printer);
 	}
 
-	public static <V> CharSequence printIterable(Iterable<V> iterable,
-			CharSequence delimiter) {
+	public static <V> CharSequence printIterable(Iterable<V> iterable, CharSequence delimiter) {
 		return printIterable(new StringBuilder(), iterable, delimiter);
 	}
 
@@ -91,13 +93,11 @@ public class StringFormatter {
 		return printIterable(new StringBuilder(), iterable);
 	}
 
-	public static <V> CharSequence printIterable(CharSequence delimiter,
-			ObjectPrinter<V> printer, V... array) {
+	public static <V> CharSequence printIterable(CharSequence delimiter, ObjectPrinter<V> printer, V... array) {
 		return printIterable(new StringBuilder(), delimiter, printer, array);
 	}
 
-	public static <V> CharSequence printIterable(CharSequence delimiter,
-			V... array) {
+	public static <V> CharSequence printIterable(CharSequence delimiter, V... array) {
 		return printIterable(new StringBuilder(), delimiter, array);
 	}
 
