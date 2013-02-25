e359f6048b06e5c3f7f8baa77ca3663cf52b6dc6
Hannes Widmoser <widmoser@gmail.com>
2013-02-25 15:19:13 +0100

    providing json utility functions for handling primitive values that may be null

---
+++ src/cat/mobilejazz/utilities/io/JSON.java
@@ -0,0 +1,35 @@
+package cat.mobilejazz.utilities.io;
+
+import java.text.ParseException;
+import java.util.Date;
+
+import org.json.JSONException;
+import org.json.JSONObject;
+
+public class JSON {
+
+	public static long getLong(JSONObject obj, String key) throws JSONException {
+		if (obj.isNull(key)) {
+			return 0L;
+		} else {
+			return obj.getLong(key);
+		}
+	}
+
+	public static boolean getBoolean(JSONObject obj, String key) throws JSONException {
+		if (obj.isNull(key)) {
+			return false;
+		} else {
+			return obj.getBoolean(key);
+		}
+	}
+
+	public static Date getTimestamp(JSONObject obj, String key) throws JSONException, ParseException {
+		if (obj.isNull(key)) {
+			return null;
+		} else {
+			return DateUtils.parseTimestamp(obj.getString(key));
+		}
+	}
+
+}
