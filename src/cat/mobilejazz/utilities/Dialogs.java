728ae36dd390a9176e8c5f45adcbae15658047a2
Hannes Widmoser <widmoser@gmail.com>
2013-03-06 11:32:29 +0100

    added a method to close a fragment after finishing a dialog

--- src/cat/mobilejazz/utilities/Dialogs.java
+++ src/cat/mobilejazz/utilities/Dialogs.java
@@ -4,11 +4,21 @@ import android.app.Activity;
 import android.app.AlertDialog;
 import android.content.Context;
 import android.content.DialogInterface;
+import android.content.DialogInterface.OnCancelListener;
 import android.content.DialogInterface.OnClickListener;
+import android.support.v4.app.FragmentActivity;
 
 public class Dialogs {
 
-	public static void showErrorDialogAndFinish(final Activity context, int titleResId, int messageResId, int buttonLabelResId) {
+	private static OnCancelListener IGNORE_CANCEL = new OnCancelListener() {
+
+		@Override
+		public void onCancel(DialogInterface dialog) {
+		}
+	};
+
+	public static void showErrorDialogAndFinish(final Activity context, int titleResId, int messageResId,
+			int buttonLabelResId) {
 		new AlertDialog.Builder(context).setMessage(messageResId).setTitle(titleResId)
 				.setPositiveButton(buttonLabelResId, new OnClickListener() {
 					@Override
@@ -18,7 +28,19 @@ public class Dialogs {
 					}
 				}).create().show();
 	}
-	
+
+	public static void showErrorDialogAndCloseFragment(final FragmentActivity context, int titleResId,
+			int messageResId, int buttonLabelResId, final String backStackName) {
+		new AlertDialog.Builder(context).setMessage(messageResId).setTitle(titleResId)
+				.setPositiveButton(buttonLabelResId, new OnClickListener() {
+					@Override
+					public void onClick(DialogInterface dialog, int which) {
+						dialog.cancel();
+						context.getSupportFragmentManager().popBackStack();
+					}
+				}).setOnCancelListener(IGNORE_CANCEL).create().show();
+	}
+
 	public static void showErrorDialog(Context context, int titleResId, int messageResId, int buttonLabelResId) {
 		new AlertDialog.Builder(context).setMessage(messageResId).setTitle(titleResId)
 				.setPositiveButton(buttonLabelResId, new OnClickListener() {
@@ -28,7 +50,7 @@ public class Dialogs {
 					}
 				}).create().show();
 	}
-	
+
 	public static void showErrorDialog(Context context, int titleResId, String message, int buttonLabelResId) {
 		new AlertDialog.Builder(context).setMessage(message).setTitle(titleResId)
 				.setPositiveButton(buttonLabelResId, new OnClickListener() {
