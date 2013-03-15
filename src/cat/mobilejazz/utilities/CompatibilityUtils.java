b882d0fb8f0b1dcf09a585b3b46dbad665bfecd4
Hannes Widmoser <widmoser@gmail.com>
2013-03-15 15:07:07 +0100

    bugfix: crashed on older versions
    just because of accessing the static field

--- src/cat/mobilejazz/utilities/CompatibilityUtils.java
+++ src/cat/mobilejazz/utilities/CompatibilityUtils.java
@@ -68,7 +68,11 @@ public class CompatibilityUtils {
 
 	public static <Params, Progress, Result> void executeParallel(AsyncTask<Params, Progress, Result> task,
 			Params... args) {
-		executeOnExecutor(task, AsyncTask.THREAD_POOL_EXECUTOR, args);
+		if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.HONEYCOMB) {
+			task.executeOnExecutor(AsyncTask.THREAD_POOL_EXECUTOR, args);
+		} else {
+			task.execute(args);
+		}
 	}
 
 }
