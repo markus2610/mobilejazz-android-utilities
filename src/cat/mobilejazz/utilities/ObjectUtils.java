db8a2f8c19bb06f162de70eb2609d41a0fc70239
Hannes Widmoser <widmoser@gmail.com>
2013-04-02 12:33:50 +0200

    added a hashCode method that can deal with null objects

--- src/cat/mobilejazz/utilities/ObjectUtils.java
+++ src/cat/mobilejazz/utilities/ObjectUtils.java
@@ -1,12 +1,20 @@
 package cat.mobilejazz.utilities;
 
 public class ObjectUtils {
-	
+
 	public static boolean equals(Object a, Object b) {
 		if (a == null) {
 			return b == null;
 		} else {
 			return a.equals(b);
+		}
+	}
+
+	public static int hashCode(Object a) {
+		if (a == null) {
+			return 0;
+		} else {
+			return a.hashCode();
 		}
 	}
 
